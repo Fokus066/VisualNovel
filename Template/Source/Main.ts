637f5b0b0343ce49eb41f3ebba4470e52da48a80
Fokus066 <romtham.gebhardt@hs-furtwangende>
2021-11-19 19:50:49 +0100

    init

--- Template/Source/Main.ts
+++ Template/Source/Main.ts
@@ -1,6 +1,49 @@
 namespace Template {
+
   export import ƒ = FudgeCore;
   export import ƒS = FudgeStory;
+
+  //define transition
+  export let transition = {
+    clock: {
+      duration: 1,
+      alpha: "./Transitions/puzzle.png",
+      edge: 1,
+    }
+  };
+
+  //define  sound
+  export let sound = {
+    backgroundtheme: "",
+    //sound
+    click: "",
+  };
+
+  export let location = {
+    bedroom: {
+      name: "Bedroom",
+      background: "./Transitions/puzzle.png",
+    }
+  };
+
+  export let character = {
+    Narrator: {
+      name: ""
+    },
+    Testperson: {
+      name: "test",
+      origin: ƒS.ORIGIN.BOTTOMCENTER,
+      pose: {
+        angry: "",
+        happy: "",
+        upset: ""
+      }
+    }
+  };
+
+  export let dataForSave = {
+
+  };
 
   console.log("FudgeStory template starting");
 
@@ -9,6 +52,9 @@ namespace Template {
     let scenes: ƒS.Scenes = [
       { scene: Scene, name: "Scene" }
     ];
+
+    let uiElement: HTMLElement = document.querySelector("[type=interface]")
+    dataForSave = ƒS.Progress.setData(dataForSave, uiElement),
 
     // start the sequence
     ƒS.Progress.go(scenes);
